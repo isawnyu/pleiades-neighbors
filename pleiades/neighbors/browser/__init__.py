6c411d01e958537d26b985a153e104c432f6be75
David Glick <david@glicksoftware.com>
2016-02-10 11:36:12 -0800

    use collective.geo instead of zgeo, remove zopeskel relics

--- pleiades/neighbors/browser/__init__.py
+++ pleiades/neighbors/browser/__init__.py
@@ -1,19 +1,18 @@
-import logging
-import math
-from shapely.geometry import asShape
-
-from Products.Five.browser import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from collective.geo.geographer.interfaces import IGeoreferenced
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import Batch
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.ZCatalog.CatalogBrains import AbstractCatalogBrain, NoBrainer
-
-from zgeo.geographer.interfaces import IGeoreferenced
+from shapely.geometry import asShape
+import logging
+import math
 
 log = logging.getLogger('pleiades.beighbors')
 
-Re = 6371000.0 # radius of earth in meters, spherical approx
+Re = 6371000.0  # radius of earth in meters, spherical approx
 F = 180.0/(math.pi*Re)
+
 
 class NeighborsView(BrowserView):
     """HTML representation of neighbors
@@ -45,26 +44,29 @@ class NeighborsView(BrowserView):
 
     def nearest(self):
         catalog = getToolByName(self.context, 'portal_catalog')
+
         class NeighborBrain(AbstractCatalogBrain, NoBrainer):
             pass
+
         cschema = catalog._catalog.schema
         scopy = cschema.copy()
-        scopy['data_record_id_']=len(cschema.keys())
-        scopy['data_record_score_']=len(cschema.keys())+1
-        scopy['data_record_normalized_score_']=len(cschema.keys())+2
-        scopy['distance']=len(cschema.keys())+3
-        scopy['center']=len(cschema.keys())+4
+        scopy['data_record_id_'] = len(cschema.keys())
+        scopy['data_record_score_'] = len(cschema.keys())+1
+        scopy['data_record_normalized_score_'] = len(cschema.keys())+2
+        scopy['distance'] = len(cschema.keys())+3
+        scopy['center'] = len(cschema.keys())+4
         NeighborBrain.__record_schema__ = scopy
         try:
             g = IGeoreferenced(self.context)
         except:
             return []
+
         def gen():
             for brain in catalog(
-                geolocation={'query': (g.bounds, 10), 'range': 'nearest'}, 
-                portal_type={'query': ['Place']},
-                sort_index='geolocation'
-                ):
+                    geolocation={'query': (g.bounds, 10), 'range': 'nearest'},
+                    portal_type={'query': ['Place']},
+                    sort_index='geolocation',
+                    ):
                 if brain.getId == self.context.getId():
                     # skip self
                     continue
@@ -74,8 +76,8 @@ class NeighborsView(BrowserView):
                 neighbor['distance'] = self.distance(brain)
                 neighbor['center'] = self.center(brain)
                 yield neighbor
+
         b_size = 20
         b_start = self.request.get('b_start', 0)
         batch = Batch(list(gen()), b_size, int(b_start), orphan=0)
         return batch
-
